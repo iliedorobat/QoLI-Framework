ca85714715c4465f6a3458c7b8431a08b9482805
Ilie Cristian Dorobat <idorobat@cisco.com>
2019-07-01 17:09:22 +0300

    changes over the way we get general parameters for occupied ratio - ParserUtils

--- src/app/java/parser/ParserUtils.java
+++ src/app/java/parser/ParserUtils.java
@@ -17,4 +17,47 @@ public class ParserUtils {
 
         return params;
     }
+
+    /**
+     * Get general parameters for consumption dataset (smokers; fruits and vegetables)
+     *
+     * @return
+     */
+    public static Map<String, String> getConsumptionParams() {
+        Map<String, String> params = getGeneralHttpParams();
+        params.put("age", "TOTAL");
+        params.put("quantile", "TOTAL");
+        params.put("sex", "T");
+        params.put("unit", "PC");
+        return params;
+    }
+
+    /**
+     * Get general parameters for self-reported unmet needs for medical/dental examination
+     *
+     * @return
+     */
+    public static Map<String, String> getUnmetHealthParams() {
+        Map<String, String> params = ParserUtils.getGeneralHttpParams();
+        params.put("age", "Y_GE16");
+        params.put("quantile", "TOTAL");
+        params.put("reason", "TOOEFW");
+        params.put("sex", "T");
+        params.put("unit", "PC");
+        return params;
+    }
+
+    /**
+     * Get general parameters for work occupation (under/over occupied ratio)
+     *
+     * @return
+     */
+    public static Map<String, String> getWorkOccupationParams() {
+        Map<String, String> params = ParserUtils.getGeneralHttpParams();
+        params.put("age", "TOTAL");
+        params.put("incgrp", "TOTAL");
+        params.put("sex", "T");
+        params.put("unit", "PC");
+        return params;
+    }
 }
