b11bf7532e8a8f58e3b3df99f5cdecee63ad3088
Ilie Cristian Dorobat <idorobat@cisco.com>
2019-08-11 09:45:59 +0300

    Environment - add the dimension generator

--- src/app/java/data/measurement/dao/impl/EnvironmentStatsImpl.java
+++ src/app/java/data/measurement/dao/impl/EnvironmentStatsImpl.java
@@ -1,12 +1,18 @@
 package app.java.data.measurement.dao.impl;
 
+import app.java.commons.MapOrder;
+import app.java.commons.MapUtils;
+import app.java.commons.MathUtils;
 import app.java.commons.constants.Constants;
+import app.java.commons.constants.EnvConst;
 import app.java.commons.constants.FileNameConst;
 import app.java.commons.constants.FilePathConst;
-import app.java.data.measurement.MeasureUtils;
 import app.java.data.measurement.dao.EnvironmentStatsDAO;
+import app.java.data.measurement.preparation.Initializer;
+import app.java.data.measurement.preparation.Preparation;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class EnvironmentStatsImpl implements EnvironmentStatsDAO {
     // The lists of queried values
@@ -24,13 +30,38 @@ public class EnvironmentStatsImpl implements EnvironmentStatsDAO {
             waterSupplyRatioPath = FilePathConst.ENVIRONMENT_PATH + FileNameConst.WATER_SUPPLY_RATIO + JSON_EXT;
 
     private static final Map<String, Number>
-            airPollutionRatio = MeasureUtils.consolidateList(AIR_POLLUTION_RATIO, airPollutionRatioPath),
-            noisePollutionRatio = MeasureUtils.consolidateList(NOISE_POLLUTION_RATIO, noisePollutionRatioPath),
-            pollutionRatio = MeasureUtils.consolidateList(POLLUTION_RATIO, pollutionRatioPath),
-            waterSupplyRatio = MeasureUtils.consolidateList(WATER_SUPPLY_RATIO, waterSupplyRatioPath);
+            initAirPollutionRatio = Initializer.initConsolidatedList(AIR_POLLUTION_RATIO, airPollutionRatioPath),
+            initNoisePollutionRatio = Initializer.initConsolidatedList(NOISE_POLLUTION_RATIO, noisePollutionRatioPath),
+            initPollutionRatio = Initializer.initConsolidatedList(POLLUTION_RATIO, pollutionRatioPath),
+            initWaterSupplyRatio = Initializer.initConsolidatedList(WATER_SUPPLY_RATIO, waterSupplyRatioPath);
 
-    public void print() {
-//        System.out.println(airPollutionRatio);
-        MeasureUtils.print(airPollutionRatioPath);
+    public Map<String, Number> generateDimensionList() {
+        Map<String, Number> consolidatedList = new TreeMap<>(new MapOrder());
+        Map<String, Number>
+//                airPollutionRatio = Preparation.prepareData(initAirPollutionRatio),
+                noisePollutionRatio = Preparation.prepareData(initNoisePollutionRatio),
+                pollutionRatio = Preparation.prepareData(initPollutionRatio);
+//                waterSupplyRatio = Preparation.prepareData(initWaterSupplyRatio)
+
+        for (int year = EnvConst.MIN_YEAR; year <= EnvConst.MAX_YEAR; year++) {
+            for (int i = 0; i < Constants.EU28_MEMBERS.length; i++) {
+                String code = Constants.EU28_MEMBERS[i];
+                String key = MapUtils.generateKey(code, year);
+
+                double reversedNoisePollutionRatio = MathUtils.percentageReverseRatio(noisePollutionRatio, key);
+                double reversedPollutionRatio = MathUtils.percentageReverseRatio(pollutionRatio, key);
+
+                double product = 1
+                        * MathUtils.percentageSafetyDouble(reversedNoisePollutionRatio)
+                        * MathUtils.percentageSafetyDouble(reversedPollutionRatio);
+                Number value = Math.log(product);
+                consolidatedList.put(key, value);
+            }
+        }
+
+//        Print.printVariation(Statistics.generateVariation(initAirPollutionRatio, true));
+//        Print.print(initAirPollutionRatio, true);
+
+        return consolidatedList;
     }
 }
