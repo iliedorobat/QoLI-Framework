b0edd4d584e248a372d69a06c9c0663b2eda167f
Ilie Cristian Dorobat <idorobat@cisco.com>
2023-11-05 20:48:13 +0200

    Remove json-stat library in favour of importing the source code

---
+++ src/no/ssb/jsonstat/v2/DatasetBuilder.java
@@ -0,0 +1,22 @@
+package no.ssb.jsonstat.v2;
+
+import java.time.Instant;
+
+/**
+ * Base builder interface for {@link Dataset}
+ */
+public interface DatasetBuilder {
+
+    DatasetBuilder withLabel(String label);
+
+    DatasetBuilder withSource(String source);
+
+    DatasetBuilder updatedAt(Instant update);
+
+    DatasetBuilder withExtension(Object jsonNodes);
+
+    DatasetValueBuilder withDimensions(Iterable<Dimension.Builder> values);
+
+    DatasetValueBuilder withDimensions(Dimension.Builder... values);
+
+}
