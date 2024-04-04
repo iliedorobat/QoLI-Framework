4aa33673ca46f554ac0c5496af15295961392df3
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-04-04 11:40:05 +0300

    Update the aggregated QoLI formula

--- src/ro/webdata/qoli/aggr/stats/dimensions/QoLIStats.java
+++ src/ro/webdata/qoli/aggr/stats/dimensions/QoLIStats.java
@@ -13,12 +13,10 @@ import ro.webdata.qoli.aggr.stats.dimensions.materialLiving.MaterialLivingStats;
 import ro.webdata.qoli.aggr.stats.dimensions.overall.OverallExperienceStats;
 import ro.webdata.qoli.aggr.stats.dimensions.safety.SafetyStats;
 import ro.webdata.qoli.aggr.stats.utils.MapUtils;
-import ro.webdata.qoli.aggr.stats.utils.MathUtils;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+
+import static ro.webdata.qoli.aggr.stats.dimensions.QoLIAggrParams.*;
 
 public class QoLIStats {
     public static Map<String, Number> generateStats(List<String> aggrList, List<String> countryCodes) {
@@ -27,42 +25,45 @@ public class QoLIStats {
                 : countryCodes;
         
         Map<String, Number> consolidatedList = new TreeMap<>(new MapOrder());
-        Map<String, Number>
-                educationStats = EducationStats.generateStats(aggrList, countryList),
-                environmentStats = EnvironmentStats.generateStats(aggrList, countryList),
-                govRightsStats = GovRightsStats.generateStats(aggrList, countryList),
-                healthStats = HealthStats.generateStats(aggrList, countryList),
-                leisureInteractStats = LeisureInteractStats.generateStats(aggrList, countryList),
-                mainActivityStats = MainActivityStats.generateStats(aggrList, countryList),
-                materialLivingStats = MaterialLivingStats.generateStats(aggrList, countryList),
-                overallExperienceStats = OverallExperienceStats.generateStats(aggrList, countryList),
-                safetyStats = SafetyStats.generateStats(aggrList, countryList);
+        Map<String, Map<String, Number>> stats = new HashMap<>() {{
+            put(EDUCATION, EducationStats.generateStats(aggrList, countryList));
+            put(ENVIRONMENT, EnvironmentStats.generateStats(aggrList, countryList));
+            put(GOVERNANCE, GovRightsStats.generateStats(aggrList, countryList));
+            put(HEALTH, HealthStats.generateStats(aggrList, countryList));
+            put(LEISURE_INTERACT, LeisureInteractStats.generateStats(aggrList, countryList));
+            put(LIVING_CONDITIONS, MaterialLivingStats.generateStats(aggrList, countryList));
+            put(MAIN_ACTIVITY, MainActivityStats.generateStats(aggrList, countryList));
+            put(OVERALL_EXPERIENCE, OverallExperienceStats.generateStats(aggrList, countryList));
+            put(SAFETY, SafetyStats.generateStats(aggrList, countryList));
+        }};
 
         for (int year = EnvConst.MIN_YEAR; year <= EnvConst.MAX_YEAR; year++) {
             for (String code : countryList) {
                 String key = MapUtils.generateKey(code, year);
+                double product = 1;
+                int counter = 0;
 
-                double education = educationStats.get(key).doubleValue();
-                double environment = environmentStats.get(key).doubleValue();
-                double govRights = govRightsStats.get(key).doubleValue();
-                double health = healthStats.get(key).doubleValue();
-                double leisureInteract = leisureInteractStats.get(key).doubleValue();
-                double mainActivity = mainActivityStats.get(key).doubleValue();
-                double materialLiving = materialLivingStats.get(key).doubleValue();
-                double overallExperience = overallExperienceStats.get(key).doubleValue();
-                double safety = safetyStats.get(key).doubleValue();
+                for (Map.Entry<String, Map<String, Number>> entry : stats.entrySet()) {
+                    Map<String, Number> dimensionValues = entry.getValue();
+                    String dimensionName = entry.getKey();
 
-                double product = 1
-                        * education
-                        * environment
-                        * govRights
-                        * health
-                        * leisureInteract
-                        * mainActivity
-                        * materialLiving
-                        * overallExperience
-                        * safety;
-                Number value = MathUtils.getLogValue(product);
+                    boolean exists = aggrList.stream()
+                            .anyMatch(indicatorName -> {
+                                // E.g.: "education"
+                                boolean isDimension = indicatorName.equals(dimensionName);
+                                // E.g.: "education:dropoutRatio"
+                                boolean isIndicator = indicatorName.startsWith(dimensionName + ":");
+
+                                return isDimension || isIndicator;
+                            });
+
+                    if (exists) {
+                        counter++;
+                        product *= dimensionValues.get(key).doubleValue();
+                    }
+                }
+
+                Number value = Math.pow(product, 1.0/counter);
                 consolidatedList.put(key, value);
             }
         }
