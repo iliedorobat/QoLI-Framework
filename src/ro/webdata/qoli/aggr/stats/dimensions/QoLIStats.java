34664b0433f1e833ae3ff7a0c022cb456e29d4c5
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-06-11 16:38:07 +0300

    Use log function to calculate QoLI insted of the geometric mean

--- src/ro/webdata/qoli/aggr/stats/dimensions/QoLIStats.java
+++ src/ro/webdata/qoli/aggr/stats/dimensions/QoLIStats.java
@@ -12,6 +12,7 @@ import ro.webdata.qoli.aggr.stats.dimensions.materialLiving.MaterialLivingStats;
 import ro.webdata.qoli.aggr.stats.dimensions.overall.OverallExperienceStats;
 import ro.webdata.qoli.aggr.stats.dimensions.safety.SafetyStats;
 import ro.webdata.qoli.aggr.stats.utils.MapUtils;
+import ro.webdata.qoli.aggr.stats.utils.MathUtils;
 
 import java.util.*;
 
@@ -51,7 +52,7 @@ public class QoLIStats {
                     }
                 }
 
-                Number value = Math.pow(product, 1.0/counter);
+                Number value = MathUtils.getLogValue(product);
                 consolidatedList.put(key, value);
             }
         }
