19d1f4d1ff3b68b9b08f98549e93bfc69630b973
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-09-18 23:35:43 +0300

    Update port number

--- src/main/java/ro/webdata/qoli/server/commons/Endpoint.java
+++ src/main/java/ro/webdata/qoli/server/commons/Endpoint.java
@@ -3,7 +3,7 @@ package ro.webdata.qoli.server.commons;
 public class Endpoint {
     public static final String URI_PATTERN = "%1$s://%2$s:%3$s/";
     public static final String DOMAIN = "localhost";
-    public static final String PORT = "3070";
+    public static final String PORT = "8080";
     public static final String PROTOCOL = "http";
     public static final String BASE_URI = String.format(URI_PATTERN, PROTOCOL, DOMAIN, PORT);
 }
