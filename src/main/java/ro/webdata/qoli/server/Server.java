d8d93b953cc9b30e1639a0cfb40af0e82a2be2e8
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-09-30 23:49:52 +0300

    Test jks

--- src/main/java/ro/webdata/qoli/server/Server.java
+++ src/main/java/ro/webdata/qoli/server/Server.java
@@ -1,129 +1,66 @@
 package ro.webdata.qoli.server;
 
-import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.NetworkListener;
-import org.glassfish.grizzly.ssl.SSLContextConfigurator;
-import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ContainerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-import ro.webdata.qoli.aggr.stats.constants.EnvConst;
-import ro.webdata.qoli.server.auth.AuthFilter;
-import ro.webdata.qoli.server.commons.Endpoint;
-import ro.webdata.qoli.server.endpoint.geo.GeoEndpoint;
-import ro.webdata.qoli.server.endpoint.stats.StatsEndpoint;
-import ro.webdata.qoli.server.endpoint.stats.collector.StatsCollectorEndpoint;
-import ro.webdata.qoli.server.endpoint.stats.config.StatsConfigEndpoint;
-
-import javax.net.ssl.SSLContext;
-import java.net.URI;
+import java.io.IOException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-// https://mkyong.com/webservices/jax-rs/jersey-hello-world-example/
+import org.glassfish.grizzly.Grizzly;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.NetworkListener;
+import org.glassfish.grizzly.http.server.ServerConfiguration;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
+
 public class Server {
-    // Base URI the Grizzly HTTP server will listen on
-    private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
-
-    public static HttpServer startServer(boolean local) throws Exception {
-        // Define the server URI and configure Grizzly with SSL
-        final URI BASE_URI = URI.create(Endpoint.BASE_URI);
-
-        // Create a resource config that registers the QoLIEndpoint JAX-RS resource
-        final ResourceConfig config = new ResourceConfig();
-
-        // Registering like this will give warnings like:
-        // WARNING: A provider ro.webdata.qoli.server.endpoint.stats.StatsEndpoint registered in SERVER runtime does not implement any provider interfaces applicable in the SERVER runtime.
-        // Due to constraint configuration problems the provider ro.webdata.qoli.server.endpoint.stats.StatsEndpoint will be ignored.
-        // But it just works and according to stackoverflow this is a bug:
-        // https://github.com/eclipse-ee4j/jersey/issues/3700
-        config.register(AuthFilter.class);
-        config.register(GeoEndpoint.class);
-        config.register(StatsEndpoint.class);
-        config.register(StatsCollectorEndpoint.class);
-        config.register(StatsConfigEndpoint.class);
-
-        if (local) {
-            config.register(new CORSFilter());
-        }
-
-        // Disable wadl because I never asked for this.
-        config.property("jersey.config.server.wadl.disableWadl", true);
-
-        LOGGER.info("Starting server...");
-
-        if (EnvConst.IS_PRODUCTION) {
-//            // Create the SSLContext
-//            SSLContext sslContext = SSLContextHelper.createSSLContext();
-//
-//            HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, config, false); // false to disable auto-start
-//
-//            // Configure SSL for the network listener
-//            NetworkListener listener = new NetworkListener("secure-listener", BASE_URI.getHost(), BASE_URI.getPort());
-//            // Create an SSLEngineConfigurator using the SSLContext
-//            SSLEngineConfigurator sslEngineConfigurator = new SSLEngineConfigurator(sslContext)
-//                    .setClientMode(false)    // Server mode
-//                    .setNeedClientAuth(false);  // Set to true if mutual TLS is required
-//            // Apply the SSLEngineConfigurator to the NetworkListener
-//            listener.setSecure(true);
-//            listener.setSSLEngineConfig(sslEngineConfigurator);
-//
-//            // Add the listener to the server
-//            server.addListener(listener);
-//
-//            System.out.println("Secure server started at: " + BASE_URI);
-//
-//            return server;
-
-            return GrizzlyHttpServerFactory.createHttpServer(
-                    BASE_URI,
-                    config,
-                    true,
-                    getSSLEngine()
-            );
-        }
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, config);
-    }
+    private static final Logger LOGGER = Grizzly.logger(Server.class);
 
     public static void main(String[] args) {
+        final HttpServer server = new HttpServer();
+        final ServerConfiguration config = server.getServerConfiguration();
+
+        // Register simple HttpHandler
+        config.addHttpHandler(new SimpleHttpHandler(), "/");
+
+        // create a network listener that listens on port 8080.
+        final NetworkListener networkListener = new NetworkListener("secured-listener", NetworkListener.DEFAULT_NETWORK_HOST,
+                NetworkListener.DEFAULT_NETWORK_PORT);
+
+        // Enable SSL on the listener
+        networkListener.setSecure(true);
+        networkListener.setSSLEngineConfig(createSslConfiguration());
+
+        server.addListener(networkListener);
         try {
-            final HttpServer httpServer = startServer(true);
-
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                try {
-                    System.out.println("Shutting down the application...");
-                    httpServer.shutdown();
-                    System.out.println("Done, exit.");
-                } catch (Exception e) {
-                    Logger.getLogger(AuthFilter.class.getName()).log(Level.SEVERE, null, e);
-                    Logger.getLogger(GeoEndpoint.class.getName()).log(Level.SEVERE, null, e);
-                    Logger.getLogger(StatsEndpoint.class.getName()).log(Level.SEVERE, null, e);
-                    Logger.getLogger(StatsCollectorEndpoint.class.getName()).log(Level.SEVERE, null, e);
-                    Logger.getLogger(StatsConfigEndpoint.class.getName()).log(Level.SEVERE, null, e);
-                }
-            }));
-
-            System.out.println(String.format("Application started.%nStop the application using CTRL+C"));
-
-            // block and wait shut down signal, like CTRL+C
-            Thread.currentThread().join();
-        } catch (Exception e) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, e);
+            // Start the server
+            server.start();
+            System.out.println("The secured server is running.\nhttps://localhost:" + NetworkListener.DEFAULT_NETWORK_PORT + "\nPress enter to stop...");
+            System.in.read();
+        } catch (IOException ioe) {
+            LOGGER.log(Level.SEVERE, ioe.toString(), ioe);
+        } finally {
+            server.shutdownNow();
         }
     }
 
-    // FIXME:
-    private static SSLEngineConfigurator getSSLEngine() {
-        SSLContextConfigurator sslConfig = new SSLContextConfigurator();
-        sslConfig.setKeyStoreFile("/home/idorobat/workspace/keystore.p12");
-        sslConfig.setKeyStorePass("SunnyDay");
-        SSLEngineConfigurator engine = new SSLEngineConfigurator(sslConfig);
-        engine.setNeedClientAuth(false);
-        engine.setClientMode(false);
-        return engine;
+    /**
+     * Initialize server side SSL configuration.
+     *
+     * @return server side {@link SSLEngineConfigurator}.
+     */
+    private static SSLEngineConfigurator createSslConfiguration() {
+        // Initialize SSLContext configuration
+        SSLContextConfigurator sslContextConfig = new SSLContextConfigurator();
+
+        ClassLoader cl = Server.class.getClassLoader();
+        // Set key store
+        URL keystoreUrl = cl.getResource("/home/idorobat/workspace/keystore.jks");
+        if (keystoreUrl != null) {
+            sslContextConfig.setKeyStoreFile(keystoreUrl.getFile());
+            sslContextConfig.setKeyStorePass("SunnyDay");
+        }
+
+        // Create SSLEngine configurator
+        return new SSLEngineConfigurator(sslContextConfig.createSSLContext(), false, false, false);
     }
 }
