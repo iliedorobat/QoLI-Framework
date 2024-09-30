a647cbd33deae290ad0a57c5f47c16477daac682
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-09-30 22:04:35 +0300

    Update SSLContextHelper

--- src/main/java/ro/webdata/qoli/server/SSLContextHelper.java
+++ src/main/java/ro/webdata/qoli/server/SSLContextHelper.java
@@ -3,26 +3,21 @@ package ro.webdata.qoli.server;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
-import java.io.File;
 import java.io.FileInputStream;
 import java.security.KeyStore;
-import java.util.logging.Logger;
 
 public class SSLContextHelper {
     // Load the keystore into the Java SSLContext
     public static SSLContext createSSLContext() throws Exception {
         // Load the KeyStore (assuming PKCS12 format)
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-
-        Logger.getLogger(SSLContext.class.getName(), new File("/etc/letsencrypt/live/qoli.webdata.ro/keystore.p12").getAbsolutePath());
-        
         try (FileInputStream keyStoreFile = new FileInputStream("/etc/letsencrypt/live/qoli.webdata.ro/keystore.p12")) {
             keyStore.load(keyStoreFile, "SunnyDay".toCharArray()); // keystorePassword
         }
 
         // Create a KeyManagerFactory
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        keyManagerFactory.init(keyStore, null); // keyPassword
+        keyManagerFactory.init(keyStore, "QuietNight".toCharArray()); // keyPassword
 
         // Create a TrustManagerFactory
         TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
