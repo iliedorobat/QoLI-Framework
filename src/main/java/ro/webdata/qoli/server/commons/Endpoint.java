a32ad84a542b39f47f213b7197234abe39d2b63f
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-10-02 12:13:47 +0300

    Add '/qoli' prefix to APIs & fix the local dev server

--- src/main/java/ro/webdata/qoli/server/commons/Endpoint.java
+++ src/main/java/ro/webdata/qoli/server/commons/Endpoint.java
@@ -7,7 +7,7 @@ public class Endpoint {
     public static final String DOMAIN = EnvState.IS_PRODUCTION ? EnvState.HOST_NAME : "localhost";
     public static final String PORT = EnvState.IS_PRODUCTION
             ? EnvState.USE_TOMCAT_SERVER ? "8080" : "8443"
-            : "";
+            : "8080";
     public static final String PROTOCOL = "https";
     public static final String BASE_URI = String.format(URI_PATTERN, PROTOCOL, DOMAIN, PORT);
 }
