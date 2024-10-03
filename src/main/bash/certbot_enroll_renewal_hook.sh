ffe94f9e9a117bd6826f978a85f55f0dbb089387
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-10-03 11:18:59 +0300

    Update enroll script

--- src/main/bash/certbot_enroll_renewal_hook.sh
+++ src/main/bash/certbot_enroll_renewal_hook.sh
@@ -1,3 +1,8 @@
  WORKPLACE_PATH="/home/idorobat/workplace"
+ AUTOMATION_PATH="$WORKPLACE_PATH/automation"
+ QOLI_FRAMEWORK_PATH="$WORKPLACE_PATH/QoLI-Framework"
 
- sudo certbot renew --deploy-hook "$WORKPLACE_PATH/QoLI-Framework/src/main/bash/certbot_post_renewal_hook.sh"
+ mkdir -p $AUTOMATION_PATH
+
+ sudo cp $QOLI_FRAMEWORK_PATH/src/main/bash/certbot_post_renewal_hook.sh $AUTOMATION_PATH/certbot_post_renewal_hook.sh
+ sudo certbot renew --deploy-hook "$AUTOMATION_PATH/certbot_post_renewal_hook.sh"
