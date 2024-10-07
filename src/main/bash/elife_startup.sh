823765d609ee34b7bd36e4d7e22bf9b7927c3ded
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-10-07 14:31:50 +0300

    Update bash scrips

--- src/main/bash/elife_startup.sh
+++ src/main/bash/elife_startup.sh
@@ -4,4 +4,4 @@ WORKPLACE_PATH="/home/idorobat/workplace"
 AUTOMATION_PATH="$WORKPLACE_PATH/automation"
 
 $AUTOMATION_PATH/elife_build.sh
-$AUTOMATION_PATH/elife_startup.sh
+$AUTOMATION_PATH/elife_run.sh
