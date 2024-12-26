2eaf151b68704a083d40436922ca788f7ecab74f
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-12-27 00:33:56 +0200

    Add copyEnv task

--- src/main/bash/elife_build.sh
+++ src/main/bash/elife_build.sh
@@ -3,4 +3,4 @@
 WORKPLACE_PATH="/home/idorobat/workplace"
 QOLI_FRAMEWORK_PATH="$WORKPLACE_PATH/QoLI-Framework"
 
-cd $QOLI_FRAMEWORK_PATH && ./gradlew clean build && ./gradlew copyProd
+cd $QOLI_FRAMEWORK_PATH && ./gradlew clean build && ./gradlew copyProd && ./gradlew copyEnv
